80b0757076bbff72c82a2ad5999b4781428c74a9
Allen Wei <allen95wei@gmail.com>
2023-08-29 11:01:21 +0800

    ChatGPT.py 現在使用ChatGPT需要先創建ChatBot物件 main.py 1. 因應ChatGPT.py更改 2. 如果初始化發生錯誤，機器人會自動停用`chat`指令，不再影響機器人運作

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,27 +3,30 @@ from OpenAIAuth import Auth0
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv("TOKEN.env")
-auth = Auth0(email_address=str(getenv("email")), password=str(getenv("password")))
-access_token = auth.get_access_token()
-chatbot = Chatbot(config={"access_token": access_token})
 
+class ChatBot:
+    def __init__(self):
+        load_dotenv("TOKEN.env")
+        auth = Auth0(email_address=str(getenv("email")), password=str(getenv("password")))
+        access_token = auth.get_access_token()
+        self.chatbot = Chatbot(config={"access_token": access_token})
 
-def chat(msg: str):
-    prev_text = ""
-    start_response = True
-    for data in chatbot.ask(msg):
-        if start_response:
-            print("Bot: ", end="")
-            start_response = False
-        responded_message = data["message"][len(prev_text):]
-        print(responded_message, end="", flush=True)
-        prev_text = data["message"]
-    return prev_text
+    def chat(self, msg: str):
+        prev_text = ""
+        start_response = True
+        for data in self.chatbot.ask(msg):
+            if start_response:
+                print("Bot: ", end="")
+                start_response = False
+            responded_message = data["message"][len(prev_text):]
+            print(responded_message, end="", flush=True)
+            prev_text = data["message"]
+        return prev_text
 
 
 if __name__ == "__main__":
+    bot = ChatBot()
     while True:
         message = input("You: ")
-        chat(message)
+        bot.chat(message)
         print()
