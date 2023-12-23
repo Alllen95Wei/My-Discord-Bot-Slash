e9c26783944fe5999f9e5f220da651e559907834
Allen Wei <allen95wei@gmail.com>
2023-12-23 19:50:22 +0800

    移除未使用到的`import`

--- cleanup.py
+++ cleanup.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-from update import kill_running_bot
-from platform import system
 
 intents = discord.Intents.all()
 bot = commands.Bot(intents=intents, help_command=None)
