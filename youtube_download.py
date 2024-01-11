0b2e105c0d9485a67514e6d642c004b912364ea9
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2024-01-11 21:56:12 +0800

    修正取得縮圖的方式

--- youtube_download.py
+++ youtube_download.py
@@ -61,8 +61,23 @@ def get_length(url):
 
 def get_thumbnail(url):
     # get thumbnail url
-    vid = get_id(url)
-    return f"https://i.ytimg.com/vi/{vid}/maxresdefault.jpg"
+    ytdl_opts = {
+        "skip_download": True,
+        "quiet": True,
+        "no_warnings": True,
+        "ignoreerrors": True,
+        "nocheckcertificate": True,
+        "restrictfilenames": True,
+        "noplaylist": True,
+        "logtostderr": False,
+        "default_search": "auto",
+        "usenetrc": False,
+        "fixup": "detect_or_warn"
+    }
+    with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        thumbnail = info_dict["thumbnail"]
+        return thumbnail
 
 
 def get_title(url):
@@ -108,4 +123,4 @@ def get_full_info(url):
 
 if __name__ == "__main__":
     # youtube_download(url=input("請貼上要下載的連結："), file_name=input("請輸入下載後的檔案名稱："))
-    print(get_full_info(input("請貼上連結：")))
+    print(get_thumbnail(input("請貼上連結：")))
