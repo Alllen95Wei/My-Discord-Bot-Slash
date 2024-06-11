fd6cc1313ba2eb29922fbeb55d2d48cfdb67c9d9
Allen95Wei <allen95wei@gmail.com>
2024-06-11 11:07:54 +0800

    feat: 在經驗值報告中的頻道與使用者名單字串加上頓號分隔
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- cogs/general.py
+++ cogs/general.py
@@ -1153,6 +1153,7 @@ class Events(commands.Cog):
                     for c in report["channels"]:
                         c = "<#" + str(c) + ">"
                         channel_str += c
+                    channel_str = channel_str.replace("><", ">、<")
                     if member.id in report["partners"]:
                         report["partners"].remove(member.id)
                     for m in report["partners"]:
@@ -1160,6 +1161,7 @@ class Events(commands.Cog):
                             continue
                         m = "<@" + str(m) + ">"
                         partner_str += m
+                    partner_str = partner_str.replace("><", ">、<")
                     embed.add_field(
                         name=f"加入過的頻道 (共{len(report['channels'])}個)",
                         value=channel_str,
