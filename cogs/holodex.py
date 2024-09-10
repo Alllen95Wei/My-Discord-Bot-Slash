b0727504d3db81d20561fd45014987923368d21b
Allen95Wei <allen95wei@gmail.com>
2024-09-10 23:07:33 +0800

    feat: 新增`/holodex clip_and_upload`，可下載選取片段後上傳YouTube
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- cogs/holodex.py
+++ cogs/holodex.py
@@ -1,17 +1,27 @@
 # coding=utf-8
 import discord
-from discord import ui, Interaction, Embed, Option, SelectOption
+from discord import (
+    ui,
+    Interaction,
+    Embed,
+    Option,
+    SelectOption,
+    ButtonStyle,
+    InputTextStyle,
+)
 from discord.ext import commands
 import os
 import zoneinfo
 from pathlib import Path
 from dotenv import load_dotenv
 import time
+import json
 from math import ceil
 
 import holodex_api
 import logger
 import youtube_download
+import youtube_uploader
 from cogs.general import Basics, Events, MUSIC_CMD_CHANNELS
 
 
@@ -31,9 +41,7 @@ class Holodex(commands.Cog):
         self.real_logger = real_logger
 
     @staticmethod
-    def slice_section_list(
-        section_list: list, page_no: int
-    ) -> tuple[tuple, list]:
+    def slice_section_list(section_list: list, page_no: int) -> tuple[tuple, list]:
         if len(section_list) <= 25:
             return (1, 1), section_list
         split_section_list = []
@@ -53,6 +61,7 @@ class Holodex(commands.Cog):
         bit_rate: int,
         sections_list: list,
         add_to_musicbot_queue: bool = False,
+        upload_to_youtube: bool = False,
     ) -> ui.View:
         view = ui.View(disable_on_timeout=True)
         # generate sections
@@ -83,6 +92,7 @@ class Holodex(commands.Cog):
             if interaction.user.id == author.id:
                 section_pos = int(menu.values[0])
                 section = sections_list[section_pos]
+                start_time = time.time()
                 embed = Embed(
                     title="片段已確認",
                     description=f"已開始下載所選取的片段。",
@@ -112,73 +122,147 @@ class Holodex(commands.Cog):
                     ),
                     inline=False,
                 )
-                embed.add_field(
-                    name="預估下載時間(依片段長度粗估)",
-                    value="約 `%d`~`%d` 秒"
-                    % (
-                        (sect["end"] - sect["start"]) / 1.9,
-                        (sect["end"] - sect["start"]) / 1.2,
-                    ),
-                    inline=False,
-                )
                 if section["art"] is not None:
                     embed.set_thumbnail(url=section["art"])
-                embed.set_footer(text="請注意，下載時間可能會比/musicdl還要久。請在數分鐘後回來查看。")
-                await interaction.edit_original_response(embed=embed, view=None)
-
-                if not add_to_musicbot_queue:
-                    metadata = {
-                        "title": section["name"],
-                        "artist": holodex_client.get_video_channel(
-                            video_instance.get_id()
-                        ),
-                        "thumbnail_url": video_instance.get_thumbnail(),
-                    }
-                else:
-                    metadata = {}
-                file_name = (
-                    f"{video_instance.get_id()}_{section['id'][-12:]}_{bit_rate}"
-                )
-                start_time = time.time()
-                result = await Basics.run_blocking(
-                    self.bot,
-                    Basics.ConfirmDownload.youtube_start_download,
-                    video_instance,
-                    metadata,
-                    bit_rate,
-                    file_name,
-                    [section["start"], section["end"]],
-                )
-                end_time = time.time()
-                time_delta = end_time - start_time
-                message = await interaction.edit_original_response(
-                    content=f"下載共花了 `{round(time_delta, 3)}` 秒 "
-                    f"(`{round((sect['end'] - sect['start'])/time_delta, 3)}` x)",
-                    file=result,
-                )
-                if add_to_musicbot_queue:
-                    file_url = message.attachments[0].url
-                    check_vc_result = await Events.check_voice_channel(
-                        self, interaction.guild
+                if upload_to_youtube:
+                    await interaction.edit_original_response(embed=embed, view=None)
+                    file_path = os.path.join(
+                        parent_dir,
+                        "ytdl",
+                        f"{video_instance.get_id()}_{section['id'][-12:]}.mp4",
                     )
-                    if isinstance(check_vc_result, str):
-                        embed = discord.Embed(
-                            title="錯誤", description="機器人自動加入語音頻道時失敗。", color=error_color
+                    await Basics.run_blocking(
+                        self.bot,
+                        video_instance.download_section_in_mp4,
+                        file_path,
+                        section["start"],
+                        section["end"],
+                    )
+                    end_time = time.time()
+                    time_delta = end_time - start_time
+                    await interaction.edit_original_response(
+                        content=f"下載共花了 `{round(time_delta, 3)}` 秒 "
+                        f"(`{round((section['end'] - section['start'])/time_delta, 3)}` x)",
+                        embed=embed,
+                        view=None,
+                    )
+                    yt_uploader = youtube_uploader.YouTubeUploader(
+                        file_path=file_path,
+                        title=f"【剪輯-測試中】{section['name']} by {video_instance.full_info['channel']}",
+                        description=f"""
+原直播：{video_instance.url}
+                        
+此剪輯片段由Allen Bot產生，使用Holodex API取得時間軸資料。
+本功能仍在測試中，且可能隨時下線並不另行通知。
+Holodex API：https://docs.holodex.net/
+                        """,
+                    )
+                    try:
+                        yt_uploader.setup_credentials()
+                        video_info = await Basics.run_blocking(
+                            self.bot,
+                            yt_uploader.upload,
                         )
-                        embed.add_field(name="錯誤訊息", value=check_vc_result)
-                        await interaction.followup.send(embed=embed)
-                    elif isinstance(check_vc_result, discord.VoiceChannel):
-                        if interaction.user in check_vc_result.members:
-                            await interaction.channel.send(
-                                "ap!p " + file_url, delete_after=0.5
+                        embed = Embed(
+                            title="上傳完成！",
+                            description=f"片段 **{section['name']}** 已經上傳至YouTube！",
+                            color=default_color,
+                        )
+                        embed.add_field(
+                            name="連結",
+                            value="https://youtu.be/" + video_info["id"],
+                            inline=False,
+                        )
+                    except KeyError:
+                        embed = Embed(
+                            title="錯誤：沒有Refresh Token",
+                            description="尚未有Refresh Token儲存在機器人內，因此無法上傳YouTube。\n"
+                            "請使用`/holodex update_token`更新Refresh Token。",
+                            color=error_color,
+                        )
+                    except Exception as e:
+                        if "Refresh token has expired or invalid." in str(e):
+                            embed = Embed(
+                                title="錯誤：Refresh Token無效",
+                                description="機器人所儲存的Refresh Token似乎已過期或失效。\n"
+                                "請使用`/holodex update_token`更新Refresh Token。",
+                                color=error_color,
                             )
                         else:
                             embed = Embed(
-                                title="錯誤：使用者不在語音頻道內",
-                                description="你必須在Allen Music Bot的語音頻道內，才可使用此功能。",
+                                title="錯誤", description="發生未知錯誤。", color=error_color
+                            )
+                            embed.add_field(
+                                name="錯誤訊息", value=f"```{e}```", inline=False
+                            )
+                    await interaction.edit_original_response(embed=embed)
+                else:
+                    embed.add_field(
+                        name="預估下載時間(依片段長度粗估)",
+                        value="約 `%d`~`%d` 秒"
+                        % (
+                            (section["end"] - section["start"]) / 1.9,
+                            (section["end"] - section["start"]) / 1.2,
+                        ),
+                        inline=False,
+                    )
+                    embed.set_footer(text="請注意，下載時間可能會比/musicdl還要久。請在數分鐘後回來查看。")
+                    await interaction.edit_original_response(embed=embed, view=None)
+
+                    if not add_to_musicbot_queue:
+                        metadata = {
+                            "title": section["name"],
+                            "artist": holodex_client.get_video_channel(
+                                video_instance.get_id()
+                            ),
+                            "thumbnail_url": video_instance.get_thumbnail(),
+                        }
+                    else:
+                        metadata = {}
+                    file_name = (
+                        f"{video_instance.get_id()}_{section['id'][-12:]}_{bit_rate}"
+                    )
+                    result = await Basics.run_blocking(
+                        self.bot,
+                        Basics.ConfirmDownload.youtube_start_download,
+                        video_instance,
+                        metadata,
+                        bit_rate,
+                        file_name,
+                        [section["start"], section["end"]],
+                    )
+                    end_time = time.time()
+                    time_delta = end_time - start_time
+                    message = await interaction.edit_original_response(
+                        content=f"下載共花了 `{round(time_delta, 3)}` 秒 "
+                        f"(`{round((section['end'] - section['start'])/time_delta, 3)}` x)",
+                        file=result,
+                    )
+                    if add_to_musicbot_queue:
+                        file_url = message.attachments[0].url
+                        check_vc_result = await Events.check_voice_channel(
+                            self, interaction.guild
+                        )
+                        if isinstance(check_vc_result, str):
+                            embed = discord.Embed(
+                                title="錯誤",
+                                description="機器人自動加入語音頻道時失敗。",
                                 color=error_color,
                             )
+                            embed.add_field(name="錯誤訊息", value=check_vc_result)
                             await interaction.followup.send(embed=embed)
+                        elif isinstance(check_vc_result, discord.VoiceChannel):
+                            if interaction.user in check_vc_result.members:
+                                await interaction.channel.send(
+                                    "ap!p " + file_url, delete_after=0.5
+                                )
+                            else:
+                                embed = Embed(
+                                    title="錯誤：使用者不在語音頻道內",
+                                    description="你必須在Allen Music Bot的語音頻道內，才可使用此功能。",
+                                    color=error_color,
+                                )
+                                await interaction.followup.send(embed=embed)
             else:
                 embed = Embed(
                     title="錯誤：非指令使用者",
@@ -191,6 +275,78 @@ class Holodex(commands.Cog):
         view.add_item(menu)
 
         return view
+
+    class TokenSubmissionWindow(ui.Modal):
+        def __init__(self):
+            super().__init__(title="提交Refresh Token")
+            self.add_item(
+                ui.InputText(
+                    style=InputTextStyle.long,
+                    label="Refresh Token",
+                    placeholder="貼上剪貼簿中的Refresh Token",
+                    required=True,
+                )
+            )
+
+        async def callback(self, interaction: discord.Interaction):
+            await interaction.response.defer(ephemeral=True)
+            token = self.children[0].value
+            if youtube_uploader.YouTubeUploader.refresh_token_is_valid(token):
+                with open(
+                    os.path.join(parent_dir, "google_client_secret.json"),
+                    "r",
+                    encoding="utf-8",
+                ) as f:
+                    data = json.load(f)
+                    data["refresh_token"] = token
+                with open(
+                    os.path.join(parent_dir, "google_client_secret.json"),
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    json.dump(data, f, ensure_ascii=False, indent=2)
+                embed = Embed(
+                    title="已儲存Refresh Token",
+                    description="此Refresh Token有效。已儲存新的Refresh Token。",
+                    color=default_color,
+                )
+            else:
+                embed = Embed(
+                    title="錯誤：Refresh Token無效",
+                    description="你所提供的Refresh Token經檢查無效。",
+                    color=error_color,
+                )
+            embed.add_field(
+                name="Refresh Token", value="||" + token + "||", inline=False
+            )
+            await interaction.followup.send(embed=embed, ephemeral=True)
+            await interaction.edit_original_response(view=None)
+
+    class TokenSubmissionButtons(ui.View):
+        def __init__(self, author: discord.User | discord.Member):
+            super().__init__(timeout=120, disable_on_timeout=True)
+            self.author = author
+            url_btn = ui.Button(
+                style=ButtonStyle.url,
+                label="點此取得新Refresh Token",
+                url="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?scope=https%3A%2F%2Fwww.googleapis"
+                ".com%2Fauth%2Fyoutube.upload&response_type=token&redirect_uri=https%3A%2F%2Falllen95wei.github.io%2F"
+                "&client_id=301053688733-0oighbmuqurd094jd9ttlb8ouoa4vjrp.apps.googleusercontent.com&service=lso&o2v"
+                "=2&ddm=0&flowName=GeneralOAuthFlow",
+            )
+            self.add_item(url_btn)
+
+        @ui.button(style=ButtonStyle.green, label="提交Refresh Token")
+        async def submit_callback(self, button, interaction: discord.Interaction):
+            if interaction.user.id == self.author.id:
+                await interaction.response.send_modal(Holodex.TokenSubmissionWindow())
+            else:
+                embed = Embed(
+                    title="錯誤：非指令使用者",
+                    description=f"你不是指令的使用者。僅有<@{self.author.id}>可使用此選單。",
+                    color=error_color,
+                )
+                await interaction.followup.send(embed=embed, ephemeral=True)
 
     HOLODEX_CMDS = discord.SlashCommandGroup(name="holodex")
 
@@ -272,7 +428,9 @@ class Holodex(commands.Cog):
                     )
                     embed.set_image(url=video.get_thumbnail())
                     if len(section_list) > 25:
-                        selected_page_no, selected_page = self.slice_section_list(section_list, page)
+                        selected_page_no, selected_page = self.slice_section_list(
+                            section_list, page
+                        )
                         embed.add_field(
                             name="頁數",
                             value=f"第 {selected_page_no[0]} / {selected_page_no[1]} 頁",
@@ -295,6 +453,103 @@ class Holodex(commands.Cog):
                 )
                 embed.add_field(name="錯誤訊息", value=f"```{str(e)}```", inline=False)
         await ctx.respond(embed=embed, view=view)
+
+    @commands.is_owner()
+    @HOLODEX_CMDS.command(
+        name="clip_and_upload", description="(開發者限定)從Holodex取得直播時間軸、下載特定片段後上傳YouTube"
+    )
+    async def holodex_clip(
+        self,
+        ctx,
+        url: Option(str, name="直播連結", description="欲抓取時間軸的直播連結(僅限YouTube)"),
+        page: Option(
+            int,
+            name="頁數",
+            description="若時間軸的片段數量超過25個，則會自動分頁",
+            min_value=1,
+            required=False,
+        ) = 1,
+    ):
+        await ctx.defer()
+        view = ui.View()
+        try:
+            video = youtube_download.Video(url)
+            section_list = holodex_client.fetch_video_timeline(video.get_id())
+            if "youtube" not in video.get_extractor():
+                embed = Embed(
+                    title="錯誤：連結不是YouTube連結",
+                    description="你所提供的連結不是YouTube的連結。請提供有效的YouTube連結。",
+                    color=error_color,
+                )
+                embed.add_field(
+                    name="Debug: Extractor",
+                    value=f"```{video.get_extractor()}```",
+                    inline=False,
+                )
+            elif len(section_list) == 0:
+                embed = Embed(
+                    title="錯誤：沒有片段",
+                    description="此影片在Holodex中沒有標記任何片段。",
+                    color=error_color,
+                )
+                embed.add_field(
+                    name="來源影片", value=f"[{video.get_title()}]({url})", inline=False
+                )
+                embed.set_image(url=video.get_thumbnail())
+            else:
+                embed = Embed(
+                    title="選擇片段",
+                    description="請從下拉式選單選擇要下載的片段。",
+                    color=default_color,
+                )
+                embed.add_field(
+                    name="來源影片", value=f"[{video.get_title()}]({url})", inline=False
+                )
+                embed.add_field(
+                    name="片段數量", value=f"`{len(section_list)}`個", inline=False
+                )
+                embed.set_image(url=video.get_thumbnail())
+                if len(section_list) > 25:
+                    selected_page_no, selected_page = self.slice_section_list(
+                        section_list, page
+                    )
+                    embed.add_field(
+                        name="頁數",
+                        value=f"第 {selected_page_no[0]} / {selected_page_no[1]} 頁",
+                        inline=False,
+                    )
+                else:
+                    selected_page = section_list
+                view = self.section_selection(
+                    ctx.author,
+                    video,
+                    0,
+                    selected_page,
+                    False,
+                    True,
+                )
+        except Exception as e:
+            embed = Embed(
+                title="錯誤",
+                description="發生未知錯誤。",
+                color=error_color,
+            )
+            embed.add_field(name="錯誤訊息", value=f"```{str(e)}```", inline=False)
+        await ctx.respond(embed=embed, view=view)
+
+    @commands.is_owner()
+    @HOLODEX_CMDS.command(
+        name="update_token", description="(開發者限定)更新上傳YouTube所用的Refresh Token"
+    )
+    async def holodex_update_token(self, ctx):
+        embed = Embed(
+            title="更新Refresh Token",
+            description="請先取得新的Refresh Token後，再點擊「提交Refresh Token」按鈕。",
+            color=default_color,
+        )
+        await ctx.respond(
+            embed=embed, view=self.TokenSubmissionButtons(ctx.author), ephemeral=True
+        )
 
 
 def setup(bot):
---
+++ youtube_uploader.py
@@ -0,0 +1,95 @@
+# coding=utf-8
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import json
+import requests
+import logging
+
+
+class YouTubeUploader:
+    def __init__(self, file_path: str, title: str, description: str):
+        self.credentials = None
+        self.file_path = file_path
+        self.body = {
+            "snippet": {
+                "title": title,
+                "description": description,
+                "categoryId": "22",
+            },
+            "status": {"privacyStatus": "unlisted", "selfDeclaredMadeForKids": False},
+        }
+        self.credentials: Credentials
+
+    @staticmethod
+    def refresh_token_is_valid(refresh_token: str) -> bool:
+        result = requests.get(
+            "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="
+            + refresh_token,
+            timeout=10,
+        )
+        return result.status_code == 200
+
+    def setup_credentials(self, refresh_token: str = None):
+        with open("google_client_secret.json", "r", encoding="utf-8") as f:
+            secret_dict = json.load(f)
+        if refresh_token is None:
+            try:
+                refresh_token = secret_dict["refresh_token"]
+            except KeyError:
+                raise Exception(
+                    """
+                \"refresh_token\" not found in \"google_client_secret.json\".
+                https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload&response_type=token&redirect_uri=https%3A%2F%2Falllen95wei.github.io%2F&client_id=301053688733-0oighbmuqurd094jd9ttlb8ouoa4vjrp.apps.googleusercontent.com&service=lso&o2v=2&ddm=0&flowName=GeneralOAuthFlow
+                """
+                )
+        elif not self.refresh_token_is_valid(refresh_token):
+            raise Exception(
+                """
+            Refresh token has expired or invalid.
+            https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload&response_type=token&redirect_uri=https%3A%2F%2Falllen95wei.github.io%2F&client_id=301053688733-0oighbmuqurd094jd9ttlb8ouoa4vjrp.apps.googleusercontent.com&service=lso&o2v=2&ddm=0&flowName=GeneralOAuthFlow
+            """
+            )
+        self.credentials = Credentials(
+            client_id=secret_dict["web"]["client_id"],
+            quota_project_id=secret_dict["web"]["project_id"],
+            token_uri=secret_dict["web"]["token_uri"],
+            client_secret=secret_dict["web"]["client_secret"],
+            token=secret_dict["token"],
+            refresh_token=refresh_token,
+        )
+
+    def upload(self) -> dict:
+        if self.credentials is None:
+            raise RuntimeError(
+                'Credentials not set. Run "setup_credentials" before uploading.'
+            )
+        else:
+            youtube = build("youtube", "v3", credentials=self.credentials)
+
+            # Docs: https://developers.google.com/youtube/v3/docs/videos/insert
+            insert_request = youtube.videos().insert(
+                part=",".join(self.body.keys()),
+                body=self.body,
+                media_body=MediaFileUpload(
+                    filename=self.file_path, chunksize=-1, resumable=True
+                ),
+            )
+
+            response = None
+            while response is None:
+                status, response = insert_request.next_chunk()
+                if response is not None:
+                    if "id" in response:
+                        logging.info(
+                            "Video id '%s' was successfully uploaded." % response["id"]
+                        )
+                        return response
+
+
+if __name__ == "__main__":
+    uploader = YouTubeUploader(
+        file_path="test.mp4", title="Test Title", description="Test Description"
+    )
+    uploader.setup_credentials(input("貼上Refresh Token: "))
+    upload_result = uploader.upload()
