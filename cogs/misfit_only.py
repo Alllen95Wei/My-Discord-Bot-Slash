fa76f47676f38fa83be99c8053dd2a8fa271b3a3
Allen95Wei <allen95wei@gmail.com>
2025-03-16 10:49:54 +0800

    fix: 修正「未開麥提示在使用者已經在語音頻道時意外觸發」的問題
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- cogs/misfit_only.py
+++ cogs/misfit_only.py
@@ -279,13 +279,13 @@ class Misfit(commands.Cog):
         after: discord.VoiceState,
     ):
         if (
-            (not member.bot)
+            not member.bot
             and (after.channel is not None)
             and (after.channel.guild.id == 1030069819199991838)  # 損友俱樂部
             and (before.channel != after.channel)
             and after.channel.id != 1096730575475318835  # AFK 頻道
-            and after.self_mute
-            or after.self_deaf
+        ) and (
+            after.self_mute or after.self_deaf
         ):
             msg = member.mention + " ，你目前__**沒有開啟麥克風**__，其他人將無法聽到你的發言。"
             await after.channel.send(msg)
